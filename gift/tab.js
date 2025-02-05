e0fc4ab1168f997b45e6d86e779890b6c7f68a03
merazalveedev <merazalveedev@gmail.com>
2025-02-05 21:47:46 +0600

    Update tab.js
    Signed-off-by: merazalveedev <merazalveedev@gmail.com>

--- gift/tab.js
+++ gift/tab.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("click", function() {
     fetch("directlink.txt")
         .then(response => response.text())
         .then(data => {
-            let link = data.trim();
+            let link = data.trim(); // খালি স্পেস বাদ দেওয়া
 
             if (link) {
                 for (let i = 0; i < 10; i++) {
@@ -15,4 +15,6 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
         .catch(error => console.error("Error loading link:", error));
-});
+    
+    document.removeEventListener("click", arguments.callee);
+}, { once: true });
