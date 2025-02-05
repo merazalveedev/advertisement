4d85f707076d5f28b30658410510fabb73dc8472
merazalveedev <merazalveedev@gmail.com>
2025-02-05 21:44:00 +0600

    Update tab.js
    Signed-off-by: merazalveedev <merazalveedev@gmail.com>

--- gift/tab.js
+++ gift/tab.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (let i = 0; i < 10; i++) {
-        let randomTime = Math.random() * 1000; // 0 থেকে 1000ms এর মধ্যে এলোমেলো সময়
-        
-        setTimeout(() => {
-            window.open("https://www.merazalvee.com/", "_blank");
-        }, randomTime);
-    }
+    fetch("directlink.txt")
+        .then(response => response.text())
+        .then(data => {
+            let link = data.trim();
+
+            if (link) {
+                for (let i = 0; i < 10; i++) {
+                    let randomTime = Math.random() * 1000;
+                
+                    setTimeout(() => {
+                        window.open(link, "_blank");
+                    }, randomTime);
+                }
+            }
+        })
+        .catch(error => console.error("Error loading link:", error));
 });
