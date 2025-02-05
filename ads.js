7bd8259cf8d0aed7c82264b36785f970dd046409
merazalveedev <merazalveedev@gmail.com>
2025-02-05 15:06:59 +0600

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
         setTimeout(() => {
             window.open("https://www.profitablecpmrate.com/v2absdmz?key=9d17a433c927bfe35cd9d568c78e80dc", "_blank");
         }, i * 1);
