40f633e1f40b6a2afa7c4b3e8077bd0c2eda2a1c
merazalveedev <merazalveedev@gmail.com>
2025-02-05 19:07:31 +0600

    Update ads.js
    Signed-off-by: merazalveedev <merazalveedev@gmail.com>

--- ads.js
+++ ads.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < 100; i++) {
+        // Generate a random delay between 0 and 1000 milliseconds (1 second)
+        const randomDelay = Math.floor(Math.random() * 1000);
+
         setTimeout(() => {
             window.open("https://www.profitablecpmrate.com/v2absdmz?key=9d17a433c927bfe35cd9d568c78e80dc", "_blank");
-        }, i * 1);
+        }, randomDelay);
     }
 });
